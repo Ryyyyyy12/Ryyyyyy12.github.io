40055779f5466e568c50d0ec4cb883fd34cbdbd6
Ryyyyyy12 <kevalin.sris@kmutt.ac.th>
2023-08-13 17:09:51 +0700

    add hand wave

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,29 @@
+import React, { useState } from "react";
 import { Stack } from "@mui/material";
-import { HashLink as Link } from "react-router-hash-link";
 import { useTheme } from "@mui/material/styles";
 
 function Home() {
   const theme = useTheme();
+  const [isWaving, setIsWaving] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsWaving(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsWaving(false);
+  };
+
+  const waveStyle = isWaving
+    ? "transform rotate-[-30deg] transition-transform duration-300 ease-in-out"
+    : "transition-transform duration-300 ease-in-out";
+
   return (
     <div
       style={{ fontFamily: "inherit", height: "100vh" }}
       className="container mx-auto flex justify-center content-center"
       id="home"
+      data-aos="fade-down"
     >
       <Stack
         direction="column"
@@ -16,31 +31,20 @@ function Home() {
         alignItems="center"
         spacing={3}
       >
-        <h1
-          className={`"text-5xl text-center transition ease-in-out delay-150 duration-300" ${
+        <h3
+          className={`text-center transition ease-in-out delay-150 duration-300 ${
             theme.palette.mode === "dark" ? "text-white" : "text-black"
           }`}
         >
-          Kevalin Srisakulpong
-        </h1>
-        <p
-          className={`"text-base italic transition ease-in-out delay-150 duration-300" ${
-            theme.palette.mode === "dark" ? "text-white" : "text-black"
-          }`}
-        >
-          Welcome to my online portfolio website :)
-        </p>
-        <Link
-          to="#about"
-          smooth
-          className={`"text-black" ${
-            theme.palette.mode === "dark" ? "text-white" : "text-black"
-          }`}
-        >
-          <button className="rounded-full border-2 border-dashed border-red w-36 h-8 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-red duration-300 hover:text-white">
-            <p className="text-sm mt-1">Reach out!</p>
-          </button>
-        </Link>
+          <span
+            className={`hand-emoji inline-block ${waveStyle}`}
+            onMouseOver={handleMouseOver}
+            onMouseLeave={handleMouseLeave}
+          >
+            👋
+          </span>
+          Hi there
+        </h3>
       </Stack>
     </div>
   );
